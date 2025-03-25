63dd10e153f500a83a6b0c55a181c6fd9513c8e6
Tech Geek <martezjmartez@outlook.com>
2025-03-25 12:48:53 -0500

    ASSIGNMENT FINISHED

--- main.js
+++ main.js
@@ -1,9 +1,50 @@
 /******************
  * YOUR CODE HERE *
- ******************
+ ******************/
+// 1. `getFirstItemFrom
+function getFirstItemFrom(arr){
+  return arr[0]
+}
 
+// 2. `getLastItemFrom
+function getLastItemFrom(arr){
+  return arr[arr.length - 1] 
+}
 
+// 3. `getIndex3
+function getIndex3(arr){
+  if (arr.length > 3) {
+  return arr[3]
+  } else {
+   return arr[arr.length-1]
+  }
 
+}
+
+// 4. `isLongList
+function isLongList(arr){
+  return arr.length >= 10
+}
+
+// 5. `firstItemIsNumber
+function firstItemIsNumber(arr){
+  if (arr && arr.length > 0 && typeof arr[0] === 'number'){
+    return true
+  }
+    return false
+}
+
+// 6.  secondCharOThirdString
+function secondCharOfThirdString(arr){
+  if (arr || arr.length < 3 ) {
+    return
+  }
+  const thirdItem = arr[2]
+  if (typeof thirdItem !== "string") {
+    return
+  }
+  
+}
 
  /********************************************
  * CODE DOWN HERE IS FOR INTERNAL USE ONLY. *
